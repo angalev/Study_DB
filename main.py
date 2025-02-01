fca6b80596752af0c3fdbf82d8a82fd59c2db51a
angalev <miha-angalev@mail.ru>
2025-02-01 12:03:00 +0300

    final edits

--- main.py
+++ main.py
@@ -1,185 +1,115 @@
 import psycopg2
 
 
-with psycopg2.connect(database="study_db", user="postgres", password="") as con:
-
 def create_tables():
-    with conn.cursor() as cur:
-        cur.execute("""
-        CREATE TABLE person_name(
-        id SERIAL PRIMARY KEY,
-        first_name VARCHAR(40) NOT NULL,
-        last_name VARCHAR(40) NOT NULL
-        );
-        """)
+    with psycopg2.connect(database="study_db", user="postgres", password="qwerty") as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+            CREATE TABLE person_name(
+            id SERIAL PRIMARY KEY,
+            first_name VARCHAR(40) NOT NULL,
+            last_name VARCHAR(40) NOT NULL
+            );
+            """)
 
-        cur.execute("""
-        CREATE TABLE phone_number(
-        id SERIAL PRIMARY KEY,
-        person_name_id INTEGER NOT NULL REFERENCES person_name(id),
-        number DECIMAL(11,0) UNIQUE
-        );
-        """)
+            cur.execute("""
+            CREATE TABLE phone_number(
+            id SERIAL PRIMARY KEY,
+            person_name_id INTEGER NOT NULL REFERENCES person_name(id),
+            number DECIMAL(11,0) UNIQUE
+            );
+            """)
 
-        cur.execute("""
-        CREATE TABLE email(
-        id SERIAL PRIMARY KEY,
-        person_name_id INTEGER NOT NULL REFERENCES person_name(id),
-        email VARCHAR(40) NOT NULL UNIQUE
-        );
-        """)
-        conn.commit()
-        print('Таблицы созданы')
+            cur.execute("""
+            CREATE TABLE email(
+            id SERIAL PRIMARY KEY,
+            person_name_id INTEGER NOT NULL REFERENCES person_name(id),
+            email VARCHAR(40) NOT NULL UNIQUE
+            );
+            """)
+            print('Таблицы созданы')
 
 def add_customer(first_name, last_name, email, phone=None):
-    with conn.cursor() as cur:
-        cur.execute("""
-        INSERT INTO person_name(first_name, last_name) VALUES(%s, %s) RETURNING id;
-        """, (first_name, last_name))
-        person_name_id = cur.fetchone()[0]
+    with psycopg2.connect(database="study_db", user="postgres", password="qwerty") as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+            INSERT INTO person_name(first_name, last_name) VALUES(%s, %s) RETURNING id;
+            """, (first_name, last_name))
+            person_name_id = cur.fetchone()[0]
 
-        cur.execute("""
-        INSERT INTO phone_number(person_name_id, number) VALUES(%s, %s);
-        """, (person_name_id, phone))
+            cur.execute("""
+            INSERT INTO phone_number(person_name_id, number) VALUES(%s, %s);
+            """, (person_name_id, phone))
 
-        cur.execute("""
-        INSERT INTO email(person_name_id, email) VALUES(%s, %s);
-        """, (person_name_id, email))
-        conn.commit()
-        print('Клиент занесён в базу')
+            cur.execute("""
+            INSERT INTO email(person_name_id, email) VALUES(%s, %s);
+            """, (person_name_id, email))
+            print('Клиент занесён в базу')
 
 def add_new_phone(person_name_id, phone_number):
-    with conn.cursor() as cur:
-        cur.execute("""INSERT INTO phone_number(person_name_id, number)
-        VALUES(%s, %s);
-        """, (person_name_id, phone_number))
-        conn.commit()
-        print('Номер телефона добавлен')
+    with psycopg2.connect(database="study_db", user="postgres", password="qwerty") as conn:
+        with conn.cursor() as cur:
+            cur.execute("""INSERT INTO phone_number(person_name_id, number)
+            VALUES(%s, %s);
+            """, (person_name_id, phone_number))
+            print('Номер телефона добавлен')
 
-def change_data(person_name_id, first_name, last_name, email_id, email, phone_id, phone=None):
-    with conn.cursor() as cur:
-        cur.execute("""
-        UPDATE person_name SET first_name=%s, last_name=%s WHERE id=%s;
-        """, (first_name, last_name, person_name_id))
-
-        cur.execute("""
-        UPDATE phone_number SET number=%s WHERE id=%s;
-        """, (phone, phone_id))
-
-        cur.execute("""
-        UPDATE email SET email=%s WHERE id=%s;
-        """, (email, email_id))
-        conn.commit()
-        if cur.rowcount > 0:
-            print('Данные о клиенте успешно изменены')
-        else:
-            print('Ошибка. Данные не заменены. Возможно клиент отсутствует в базе')
+def change_data(person_name_id=None, first_name=None, last_name=None,
+                email_id=None, email=None, phone_id=None, phone=None):
+    with psycopg2.connect(database="study_db", user="postgres", password="qwerty") as conn:
+        with conn.cursor() as cur:
+            if first_name:
+                cur.execute("""
+                UPDATE person_name SET first_name=%s WHERE id=%s;
+                """, (first_name, person_name_id))
+            if last_name:
+                cur.execute("""
+                UPDATE person_name SET last_name=%s WHERE id=%s;
+                """, (last_name, person_name_id))
+            if phone:
+                cur.execute("""
+                UPDATE phone_number SET number=%s WHERE id=%s;
+                """, (phone, phone_id))
+            if email:
+                cur.execute("""
+                UPDATE email SET email=%s WHERE id=%s;
+                """, (email, email_id))
+            if cur.rowcount > 0:
+                print('Данные о клиенте успешно изменены')
+            else:
+                print('Ошибка. Данные не заменены. Возможно клиент отсутствует в базе')
 
 def dell_phone(phone_id):
-    with conn.cursor() as cur:
-        cur.execute("""
-        DELETE FROM phone_number WHERE id=%s;
-        """, (phone_id, ))
-        conn.commit()
-        if cur.rowcount > 0:
-            print('Номер телефона успешно удалён')
-        else:
-            print('Ошибка. Данные не изменены. Возможно номер телефона отсутствует в базе')
+    with psycopg2.connect(database="study_db", user="postgres", password="qwerty") as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+            DELETE FROM phone_number WHERE id=%s;
+            """, (phone_id, ))
+            if cur.rowcount > 0:
+                print('Номер телефона успешно удалён')
+            else:
+                print('Ошибка. Данные не изменены. Возможно номер телефона отсутствует в базе')
 
 def delete_customer(person_name_id):
-    with conn.cursor() as cur:
-        cur.execute("""
-        DELETE FROM phone_number WHERE person_name_id=%s;
-        """, (person_name_id, ))
+    with psycopg2.connect(database="study_db", user="postgres", password="qwerty") as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+            DELETE FROM phone_number WHERE person_name_id=%s;
+            """, (person_name_id, ))
 
-        cur.execute("""
-        DELETE FROM email WHERE person_name_id=%s;
-        """, (person_name_id, ))
+            cur.execute("""
+            DELETE FROM email WHERE person_name_id=%s;
+            """, (person_name_id, ))
 
-        cur.execute("""
-        DELETE FROM person_name WHERE id=%s;
-        """, (person_name_id, ))
-        conn.commit()
-        if cur.rowcount > 0:
-            print('Данные о клиенте успешно удалены')
-        else:
-            print('Ошибка. Запись отсутствует в базе')
+            cur.execute("""
+            DELETE FROM person_name WHERE id=%s;
+            """, (person_name_id, ))
+            if cur.rowcount > 0:
+                print('Данные о клиенте успешно удалены')
+            else:
+                print('Ошибка. Запись отсутствует в базе')
 
-def search_customer(search_request: str):
-    if search_request.isdigit():
-        with conn.cursor() as cur:
-            cur.execute("""
-            SELECT pn.first_name, pn.last_name, p.number, e.email
-            FROM person_name pn
-            RIGHT JOIN phone_number p
-            ON pn.id = p.person_name_id
-            RIGHT JOIN email e
-            ON pn.id = e.person_name_id
-            WHERE e.person_name_id = 
-            (
-                SELECT p.person_name_id
-                FROM phone_number p
-                WHERE CAST(p.number AS TEXT) = %s
-            ) 
-            OR p.person_name_id =
-            (
-                SELECT p.person_name_id
-                FROM phone_number p
-                WHERE CAST(p.number AS TEXT) = %s
-            );
-            """, (search_request, search_request))
-            result = cur.fetchall()
-    if '@' in search_request:
-        with conn.cursor() as cur:
-            cur.execute("""
-            SELECT pn.first_name, pn.last_name, p.number, e.email
-            FROM person_name pn
-            RIGHT JOIN phone_number p
-            ON pn.id = p.person_name_id
-            RIGHT JOIN email e
-            ON pn.id = e.person_name_id
-            WHERE e.person_name_id = 
-            (
-                SELECT e.person_name_id
-                FROM email e
-                WHERE e.email = %s
-            ) 
-            OR p.person_name_id =
-            (
-                SELECT e.person_name_id
-                FROM email e
-                WHERE e.email = %s
-            );
-            """, (search_request, search_request))
-            result = cur.fetchall()
-    if len(search_request.split()) == 1 and (not
-        search_request.isdigit()) and ("@" not in search_request):
-        with conn.cursor() as cur:
-            cur.execute("""
-            SELECT pn.first_name, pn.last_name, p.number, e.email
-            FROM person_name pn
-            RIGHT JOIN phone_number p
-            ON pn.id = p.person_name_id
-            RIGHT JOIN email e
-            ON pn.id = e.person_name_id
-            WHERE pn.first_name ILIKE %s ESCAPE '';
-            """, (search_request + '%', ))
-            result = cur.fetchall()
-    if len(search_request.split()) > 1:
-        with conn.cursor() as cur:
-            cur.execute("""
-            SELECT pn.first_name, pn.last_name, p.number, e.email
-            FROM person_name pn
-            RIGHT JOIN phone_number p
-            ON pn.id = p.person_name_id
-            RIGHT JOIN email e
-            ON pn.id = e.person_name_id
-            WHERE LOWER(pn.first_name) = %s
-            AND pn.last_name ILIKE %s ESCAPE '';
-            """, (search_request.split()[0].lower(),
-                  search_request.split(' ', 1)[1] + '%'))
-            result = cur.fetchall()
-# print
+def print_search_result(result):
     if len(result) == 0:
         print('Клиент не найден')
     else:
@@ -194,77 +124,101 @@ def search_customer(search_request: str):
             if result[i][3] == result[i-1][3]:
                 break
 
-if __name__ = "__main__"
+def search_customer(first_name='%', last_name='%',
+                email='%', phone='%'):
+    with psycopg2.connect(database="study_db", user="postgres", password="qwerty") as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+            SELECT pn.first_name, pn.last_name, p.number, e.email
+            FROM person_name pn
+            JOIN phone_number p
+            ON pn.id = p.person_name_id
+            JOIN email e
+            ON pn.id = e.person_name_id
+            AND pn.first_name ILIKE %s
+            AND pn.last_name ILIKE %s
+            AND e.email ILIKE %s
+            AND COALESCE(CAST(p.number AS TEXT), '-') ILIKE %s;
+            """, (first_name + '%', last_name + '%',
+                email + '%', str(phone) + '%'))
+            result = cur.fetchall()
+            print_search_result(result)
 
-try:
-    create_tables()
-except psycopg2.errors.DuplicateTable:
-    conn.rollback()
-    print('Ошибка создания таблиц')
-input('Press enter to continue')
 
-try:
-    add_customer('Ivan', 'Ivanov',
-             'ivan@pochta.ru', 89083457284)
-except psycopg2.errors.UniqueViolation:
-    conn.rollback()
-    print('Ошибка добавления. Клиент уже существуют')
-input('Press enter to continue')
 
-try:
-    add_customer('Petr', 'Petrov',
-             'petr@mail.ru', 81112223334)
-except psycopg2.errors.UniqueViolation:
-    conn.rollback()
-    print('Ошибка добавления. Клиент уже существуют')
-input('Press enter to continue')
+if __name__ == "__main__":
+# CREATE TABLEs
+    try:
+        create_tables()
+    except psycopg2.errors.DuplicateTable:
+        conn.rollback()
+        print('Ошибка создания таблиц')
+    input('Press enter to continue')
+# ADD Customers
+    try:
+        add_customer('Ivan', 'Ivanov',
+                 'ivan@pochta.ru', 89083457284)
+    except psycopg2.errors.UniqueViolation:
+        conn.rollback()
+        print('Ошибка добавления. Клиент уже существуют')
+    input('Press enter to continue')
 
-try:
-    add_customer('Sergey', 'Sergeev',
-             'sergey@pochta-mail.ru', 89090001199)
-except psycopg2.errors.UniqueViolation:
-    conn.rollback()
-    print('Ошибка добавления. Клиент уже существуют')
-input('Press enter to continue')
+    try:
+        add_customer('Petr', 'Petrov',
+                 'petr@mail.ru', 81112223334)
+    except psycopg2.errors.UniqueViolation:
+        conn.rollback()
+        print('Ошибка добавления. Клиент уже существуют')
+    input('Press enter to continue')
 
-try:
-    add_customer('Olga', 'Buzova',
-             'buzovastar@super.ru')
-except psycopg2.errors.UniqueViolation:
-    conn.rollback()
-    print('Ошибка добавления. Клиент уже существуют')
-input('Press enter to continue')
+    try:
+        add_customer('Sergey', 'Sergeev',
+                 'sergey@pochta-mail.ru', 89090001199)
+    except psycopg2.errors.UniqueViolation:
+        conn.rollback()
+        print('Ошибка добавления. Клиент уже существуют')
+    input('Press enter to continue')
 
-try:
-    add_new_phone(1, 85557771212)
-except psycopg2.errors.UniqueViolation:
-    conn.rollback()
-    print('Ошибка добавления. Номер телефона уже существуют')
-input('Press enter to continue')
+    try:
+        add_customer('Olga', 'Buzova',
+                 'buzovastar@super.ru')
+    except psycopg2.errors.UniqueViolation:
+        conn.rollback()
+        print('Ошибка добавления. Клиент уже существуют')
+    input('Press enter to continue')
+# ADD Customer's phone
+    try:
+        add_new_phone(1, 85557771212)
+    except psycopg2.errors.UniqueViolation:
+        conn.rollback()
+        print('Ошибка добавления. Номер телефона уже существуют')
+    input('Press enter to continue')
+# Change any customer's data
+    change_data(person_name_id=3, first_name='Mikhail', last_name=None,
+            email_id=None, email=None, phone_id=None, phone=None)
+    input('Press enter to continue')
+# Delete phone
+    dell_phone(2)
+    input('Press enter to continue')
+# Delete customer
+    delete_customer(2)
+    input('Press enter to continue')
+# Search scenarios
+    search_customer(first_name='olga')
+    input('Press enter to continue')
 
-change_data(3, 'Mikhail', 'Angalev',
-        3, 'mikhail@aa.com', 3, 1533325562)
-input('Press enter to continue')
+    search_customer(first_name='mikhail', last_name= 's')
+    input('Press enter to continue')
 
-dell_phone(2)
-input('Press enter to continue')
+    search_customer(phone='89083457284')
+    input('Press enter to continue')
 
-delete_customer(2)
-input('Press enter to continue')
+    search_customer(first_name='IVAN')
+    input('Press enter to continue')
 
-search_customer('olga')
-input('Press enter to continue')
+    search_customer(email='sergey@')
+    input('Press enter to continue')
 
-search_customer('mikhail A')
-input('Press enter to continue')
-
-search_customer('89083457284')
-input('Press enter to continue')
-
-search_customer('mikhail@aa.com')
-input('Press enter to continue')
-
-search_customer('Donald Trump')
-conn.close()
-print('Работа завершена')
+    search_customer(first_name='Donald', last_name= 'Trump')
+    print('Работа завершена')
 
